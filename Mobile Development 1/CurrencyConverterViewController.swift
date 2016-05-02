f73c388e97ee8f15f77d37bebdb04dcc932417b6
Aliaksandr <polycirrus@if-you-see-this-then-your-dns-resolver-is-wrong.telecom.by>
2016-05-02 19:27:11 +0300

    Finish task 3

--- Mobile Development 1/CurrencyConverterViewController.swift
+++ Mobile Development 1/CurrencyConverterViewController.swift
@@ -1,16 +1,22 @@
-//
-//  CurrencyConverterViewController.swift
-//  Mobile Development 1
-//
-//  Created by Александр on 2/29/16.
-//
-//
-
 import UIKit
 
-class CurrencyConverterViewController: UIViewController {
+class CurrencyConverterViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
 
     let converter = CurrencyConverter()
+    var value: Double?
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func onTextBoxEditingChanged(sender: UITextField) {
+        if let text = sender.text {
+            value = Double(text)
+        }
+        else {
+            value = nil
+        }
+        
+        tableView.reloadData()
+    }
     
     override func viewDidLoad() {
         return
@@ -18,6 +24,36 @@ class CurrencyConverterViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (section == 0) {
+            return converter.currencies.count
+        }
+        return 0;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Value2, reuseIdentifier: nil)
+        cell.textLabel?.textAlignment = NSTextAlignment.Left
+        
+        if let unwrappedValue = value {
+            let convertedValue = converter.ConvertTo(unwrappedValue, currency: converter.currencies[indexPath.row])
+            cell.textLabel?.text = String(convertedValue)
+        }
+        else {
+            cell.textLabel?.text = ""
+        }
+        cell.detailTextLabel?.numberOfLines = 0
+        cell.detailTextLabel?.lineBreakMode = NSLineBreakMode.ByWordWrapping
+        cell.detailTextLabel?.text = converter.currencies[indexPath.row].name
+        
+        return cell
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
     
 }
